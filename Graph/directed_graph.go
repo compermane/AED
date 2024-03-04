2f464d92c774ac74f15729547cc4e013f18e6c48
compermane <comper.mane@gmail.com>
2024-03-03 23:28:36 -0300

    Atualização em grafo

--- Graph/directed_graph.go
+++ Graph/directed_graph.go
@@ -17,40 +17,85 @@ import "fmt"
 */
 
 type DiGraph struct {
-	Nodes map[string]*Node
+	Nodes 	[]*Node
+	Edges 	[]*Edge
 }
 
 type Node struct {
-	NodeID 		string
-	Distances 	map[string]int
+	ID		string
 }
 
-func CreateNode(nodeID string, distances map[string]int) *Node {
+type Edge struct {
+	origin 	*Node
+	destiny	*Node
+	weight	float64
+}
+
+func CreateNode(id string) *Node {
 	return &Node {
-		NodeID: nodeID,
-		Distances: distances,
+		ID: id,
 	}
 }
 
-func CreateDiGraph(nodes map[string]*Node) *DiGraph {
+func CreateEdge(nodeA, nodeB *Node, weight float64) *Edge {
+	return &Edge {
+		origin: nodeA,
+		destiny: nodeB,
+		weight: weight,
+	}
+}
+
+func CreateGraph() *DiGraph {
+	nodes := make([]*Node, 0)
+	edges := make([]*Edge, 0)
+
 	return &DiGraph {
 		Nodes: nodes,
+		Edges: edges,
 	}
+}
+
+func (dg *DiGraph) AddNode(newNode *Node) {
+	dg.Nodes = append(dg.Nodes, newNode)
+}
+
+func (dg *DiGraph) AddEdge(nodeA, nodeB *Node, weight float64) {
+	newEdge := CreateEdge(nodeA, nodeB, weight)
+	dg.Edges = append(dg.Edges, newEdge)
 }
 
 func (dg *DiGraph) PrintDiGraph() {
 	for _, node := range dg.Nodes {
-		fmt.Printf("%v: {", node.NodeID)
+		fmt.Printf("%v: {", node.ID)
+		var currentEdges []*Edge
 
-		lastNode := len(node.Distances) - 1
+		for _, edge := range dg.Edges {
+			if edge.origin == node {
+				currentEdges = append(currentEdges, edge)
+			}
+		}
+
+		n := len(currentEdges) - 1
 		i := 0
-		for dest, dist := range node.Distances {
-			fmt.Printf("%v: %v", dest, dist)
-			if i < lastNode {
+		for _, edge := range currentEdges {
+			fmt.Printf("%v: %v", edge.destiny.ID, edge.weight)
+			if i < n {
 				fmt.Printf(", ")
 			}
+			i++
 		}
 		fmt.Printf("}\n")
 	}
 }
 
+func (dg *DiGraph) Neighbors(origin *Node) []*Node {
+	neighbors := make([]*Node, 0)
+
+	for _, edge := range dg.Edges {
+		if edge.origin == origin {
+			neighbors = append(neighbors, edge.destiny)
+		}
+	}
+
+	return neighbors
+}
